8c0bac99d6c96b136b7e89f59b23d125b25cdbde
lazy_enzo <263153193+Aseel012@users.noreply.github.com>
2026-08-24 07:31:55 +0530

    Remove blank line in CreateTagDto
    Remove unnecessary blank line at the start of CreateTagDto.

--- src/tags/dto/create-tag.dto.ts
+++ src/tags/dto/create-tag.dto.ts
@@ -1,7 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, MaxLength } from 'class-validator';
 
-
 export class CreateTagDto {
   @ApiProperty({ example: 'Spicy' })
   @IsString()
